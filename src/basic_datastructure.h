2d72c492a23ef7dd6796a7a8211b447d5430252c
Aishwarya Pawar <arpawar@andrew.cmu.edu>
2019-05-12 14:23:51 -0400

    Update basic_datastructure.h

--- src/basic_datastructure.h
+++ src/basic_datastructure.h
@@ -4,6 +4,7 @@ grid points lie inside/outside the geometry. For each point in the grid a ray
 starts from the grid point and in the x-direction to check for intersection
 with the geometry. If the points are inside, the number of intersections are odd,
 else the number of intersections are even.
+
 This header function defines the structure of a vertex coordinate vertex3D:
 i.e. x,y,z coordinates in the 3D space.
 The struct variable tri defines the index of the triangular face vertices
@@ -38,6 +39,9 @@ public:
 	CUDA_CALLABLE_MEMBER ~vertex3D(){};
 };
 
+// Struct tri defining triangle face
+//tri variable consists of the three indices of the vertices in the face: v[3]
+// and the normal vector to the face: n
 class tri
 {
 public:
@@ -51,7 +55,4 @@ public:
 
 	CUDA_CALLABLE_MEMBER ~tri(){};
 };
-
-
-
 #endif
